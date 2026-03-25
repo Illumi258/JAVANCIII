d3d7317e402d8eb4551f0ad1e3a83fd4c38c3fab
Benny Dave F. Samuyag <94389123+Illumi258@users.noreply.github.com>
2026-03-25 05:52:22 +0000

    TASK2JAVA

---
+++ javatask2/javatask2.java
@@ -0,0 +1,15 @@
+public class javatask2 {
+    public static void main(String[] args) {
+
+        char letter = 'H';
+        byte num1 = 3;
+        short num2 = 1;
+        int num3 = 10;
+        float num4 = 2.0f;
+        boolean isTrue = true;
+
+        String output = "" + letter + num1 + num2 + num3 + " world " + num4 + " " + isTrue;
+
+        System.out.println(output);
+    }
+}
