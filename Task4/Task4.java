275c4ea28e89c6eb982bfdd83487dd53fa3f3c11
Benny Dave F. Samuyag <94389123+Illumi258@users.noreply.github.com>
2026-04-07 11:48:10 +0800

    Update Task4.java

--- Task4/Task4.java
+++ Task4/Task4.java
@@ -4,15 +4,15 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ask user for input
+    
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Use StringBuilder to reverse the string
+       
         StringBuilder sb = new StringBuilder(input);
         String reversed = sb.reverse().toString();
 
-        // Check if original and reversed strings are equal
+       
         if (input.equals(reversed)) {
             System.out.println("The input string is a palindrome.");
         } else {
