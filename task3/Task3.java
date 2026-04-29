332d4447294ff07192f68f0817b26dfc1ec271f7
Benny Dave F. Samuyag <94389123+Illumi258@users.noreply.github.com>
2026-04-29 09:25:12 +0000

    task6

--- task3/Task3.java
+++ task3/Task3.java
@@ -1,15 +1,15 @@
 public class Task3 {
     public static void main(String[] args) {
 
-        // Changed variables so all if conditions become true
+        
         String a = new String("Wow!");
         String b = "Wow";
         String c = new String("Wow");
         String d = c;
 
-        boolean b1 = a != b;              // true
-        boolean b2 = d.equals(b + "");    // true
-        boolean b3 = !c.equals(a);        // true
+        boolean b1 = a != b;              
+        boolean b2 = d.equals(b + "");    
+        boolean b3 = !c.equals(a);        
 
         if (b1 && b2 && b3) {
             System.out.println("Success!");
