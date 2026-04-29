440852d69ecbcf8148fec433c215f327ed211d01
Benny Dave F. Samuyag <94389123+Illumi258@users.noreply.github.com>
2026-04-29 10:50:04 +0000

    task16

---
+++ task16/Task16.java
@@ -0,0 +1,44 @@
+interface Animal {
+    boolean feed(boolean timeToEat);
+    void groom();
+    void pet();
+}
+
+class Gorilla implements Animal {
+
+    @Override
+    public boolean feed(boolean timeToEat) {
+        // put gorilla food into cage
+        if (timeToEat) {
+            System.out.println("Gorilla is being fed.");
+            return true;
+        } else {
+            System.out.println("Gorilla is not hungry right now.");
+            return false;
+        }
+    }
+
+    @Override
+    public void groom() {
+        // lather, rinse, repeat
+        System.out.println("Gorilla is being groomed.");
+    }
+
+    @Override
+    public void pet() {
+        // pet at your own risk
+        System.out.println("Warning: Petting a gorilla is dangerous!");
+    }
+}
+
+public class Task16 {
+
+    public static void main(String[] args) {
+
+        Gorilla g = new Gorilla();
+
+        g.feed(true);
+        g.groom();
+        g.pet();
+    }
+}
